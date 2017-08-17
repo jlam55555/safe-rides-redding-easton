f73b3cd10625cf3a563dcc994f4cd1e6461a0bc1
Jonathan Lam <jlam55555@gmail.com>
2017-08-17 08:50:03 -0400

    add extra padding for standalone

--- public/script.js
+++ public/script.js
@@ -3,6 +3,9 @@ $(function() {
   // make web app standalone (i.e., not open links in safari)
   // src: https://gist.github.com/irae/1042167 (condensed version)
   (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+  if(("standalone" in document.window.navigator) && document.window.navigator["standalone"]) {
+    $("#header").addClass("webApp");
+  }
 
   // scrolling details
   $("body").on("touchmove", function(event) {
